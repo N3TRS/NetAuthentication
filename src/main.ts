a6b9c746db567288bea01f0da24e0090433288b0
Julian Lopez <julian.lopez-b@mail.escuelaing.edu.co>
2026-04-03 17:35:10 -0500

    adding cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  app.enableCors({
+    origin: configService.get<string>('FRONTEND_URL', 'http://localhost:3001'),
+    credentials: true,
+  });
+  
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
